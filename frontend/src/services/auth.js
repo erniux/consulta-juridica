f1728ec3c9770942077d38b33e2b41f6b97624af
erniux <erna.tercero@hotmail.com>
2026-04-11 15:40:20 -0600

    error en prod de autenticacion

--- frontend/src/services/auth.js
+++ frontend/src/services/auth.js
@@ -2,7 +2,7 @@ import { api } from "./api";
 
 export const authService = {
   async login(username, password) {
-    const response = await api.post("/auth/login/", { username, password });
+    const response = await api.post("/api/auth/login/", { username, password });
     return response.data;
   },
   async me() {
